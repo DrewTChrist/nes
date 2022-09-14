376cccce1339434591fff311917f01f8648d061e
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-09-14 13:29:41 -0700

    Add tests for cmp absolute

--- tests/cpu_unit.rs
+++ tests/cpu_unit.rs
@@ -843,16 +843,43 @@ mod cpu {
     #[test]
     fn _cd_pos() {
         // cmp absolute
+        // A >= M
+        let program: [u8; 5] = [0xcd, 0x03, 0x80, 0x19, 0x00];
+        let mut cpu = Cpu::new();
+        cpu.load_program(program);
+        cpu.reg.a = 0x32;
+        cpu.tick();
+        assert!(cpu.reg.p & 0b0000_0001 != 0);
+        assert!(cpu.reg.p & 0b0000_0010 == 0);
+        assert!(cpu.reg.p & 0b1000_0000 == 0);
     }
 
     #[test]
     fn _cd_eq() {
         // cmp absolute
+        // A = M
+        let program: [u8; 5] = [0xcd, 0x03, 0x80, 0x32, 0x00];
+        let mut cpu = Cpu::new();
+        cpu.load_program(program);
+        cpu.reg.a = 0x32;
+        cpu.tick();
+        assert!(cpu.reg.p & 0b0000_0001 != 0);
+        assert!(cpu.reg.p & 0b0000_0010 != 0);
+        assert!(cpu.reg.p & 0b1000_0000 == 0);
     }
 
     #[test]
     fn _cd_neg() {
         // cmp absolute
+        // A < M
+        let program: [u8; 5] = [0xcd, 0x03, 0x80, 0x32, 0x00];
+        let mut cpu = Cpu::new();
+        cpu.load_program(program);
+        cpu.reg.a = 0x19;
+        cpu.tick();
+        assert!(cpu.reg.p & 0b0000_0001 == 0);
+        assert!(cpu.reg.p & 0b0000_0010 == 0);
+        assert!(cpu.reg.p & 0b1000_0000 != 0);
     }
 
     #[test]
